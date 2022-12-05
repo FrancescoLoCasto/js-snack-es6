dbf7c0fa9f2a005fb48a5f32d4fdc46c6f09f616
MyUsername <my@email.com>
2022-12-05 12:32:20 +0100

    Creation of zucchine list

--- Snack_3/js/Script.js
+++ Snack_3/js/Script.js
@@ -6,3 +6,73 @@
  * Calcola quanto pesano tutte le zucchine.
  *
  */
+
+
+ const zucchine = [
+
+   {
+       varieta : "Zucchina",
+       peso : 56,
+       lunghezza : 12
+
+   },
+
+   {
+       varieta: "Zucchina",
+       peso: 25,
+       lunghezza: 23
+
+   },
+   {
+       varieta: "Zucchina",
+       peso: 10,
+       lunghezza: 32
+
+   },
+   {
+       varieta: "Zucchina",
+       peso: 43,
+       lunghezza: 26
+
+   },
+   {
+       varieta: "Zucchina",
+       peso: 38,
+       lunghezza: 567
+
+   },
+   {
+       varieta: "Zucchina",
+       peso: 19,
+       lunghezza: 122
+
+   },
+   {
+       varieta: "Zucchina",
+       peso: 29,
+       lunghezza: 786
+
+   },
+   {
+       varieta: "Zucchina",
+       peso: 2,
+       lunghezza: 564
+
+   },
+   {
+       varieta: "Zucchina",
+       peso: 78,
+       lunghezza: 12
+
+   },
+   {
+       varieta: "Zucchina",
+       peso: 12,
+       lunghezza: 54
+
+   }
+
+
+]
+
+let tot = 0
