7b8f1643bc00dfae46a8b4487a24851f1b8fcffd
MyUsername <my@email.com>
2022-12-05 11:40:30 +0100

    Finish exercise

--- js/Script.js
+++ js/Script.js
@@ -1,3 +1,15 @@
 //1.** Creare un oggetto che descriva una studentessa o uno studente, con le seguenti proprietà: `nome`, `cognome` e `età`.
 //2.** Stampare a schermo attraverso un ciclo `for-in` tutte le proprietà dell'oggetto.
 //3. Creare un array di oggetti di studenti (ossìa oggetti con le proprietà sopra descritte) e mostriamoli.
+
+const student = {
+   nome : "Francesco",
+   cognome : "Lo Casto",
+   età : "21"
+ }
+
+ console.log(student)
+
+ for(let key in student){
+   console.log(key + ": " + student[key]);
+}
