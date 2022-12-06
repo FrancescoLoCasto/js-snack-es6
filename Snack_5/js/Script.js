4dc2e46b1936b97110e6bdec1e4104561f6e4e55
MyUsername <my@email.com>
2022-12-06 11:55:29 +0100

    Creation Snack_5

---
+++ Snack_5/js/Script.js
@@ -0,0 +1,9 @@
+/***
+ * 
+Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un'età, creiamone almeno una decina.
+
+Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
+
+ */ 
+
